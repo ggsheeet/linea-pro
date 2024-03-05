5584352fe3894f09829eeba17cbbfe3a5ee0df45
Luis Gzz <ggcryptodoc@gmail.com>
2024-03-04 20:25:30 -0600

    didin't work, trying different height

--- src/components/hero/heroMain/heroCTA/index.tsx
+++ src/components/hero/heroMain/heroCTA/index.tsx
@@ -5,7 +5,7 @@ import styles from './HeroCTA.module.css'
 export const HeroCTA = () => {
 	return (
 		<div className={styles.hero_text}>
-			<h1 data-aos='fade-up' data-aos-offset='300'>
+			<h1 data-aos='fade-up' data-aos-offset='-100'>
 				"El cacao debe ser nuevamente el protagonista del chocolate, no el azúcar"
 			</h1>
 			<span data-aos='fade-up' data-aos-offset='-100'>-Ana Parizot</span>
